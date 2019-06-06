c39705b0f700726eda5c4a8c484162b29410bab6
agcy1210 <agcy7320@gmail.com>
2019-06-06 19:35:56 +0530

    permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 from . import serializers
-from rest_framework import status
 from . import models
+from . import permissions
 
 
 class HelloApiView(APIView):
@@ -103,8 +104,11 @@ class HelloViewSet(viewsets.ViewSet):
 
         return Response({'http_method': "DELETE"})
 
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handles creating, reading & updating profiles"""
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
